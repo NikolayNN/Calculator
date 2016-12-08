d292826021becfd280fc97905dc8bcd7ffcdcbbf
Nikolay <Horushko.Nikolay@gmail.com>
2016-12-08 22:35:22 +0300

    task 4.2 done

---
+++ chapter_001/src/main/java/ru/nhorushko/Factorial.java
@@ -0,0 +1,26 @@
+package ru.nhorushko;
+
+/**
+ * The class contains method for retrieve factorial.
+ */
+public class Factorial {
+    /**
+     * The method retrieve factorial from {@param parameter}.
+     * If can't retrieve factorial {@throws RuntimeException}.
+     *
+     * @return factorial
+     */
+    public int retrieveFactorial(int parameter) throws RuntimeException {
+        if (parameter == 0) {
+            return 1;
+        }
+        if (parameter < 0) {
+            throw new RuntimeException("can't retrieve factorial from negative number.");
+        }
+        int result = 1;
+        for (int i = 1; i <= parameter; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/nhorushko/FactorialTest.java
@@ -0,0 +1,50 @@
+package ru.nhorushko;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * The Test contains tests for {@link Factorial}.
+ */
+public class FactorialTest {
+    /**
+     * entity for test.
+     */
+    private Factorial factorial;
+
+    /**
+     * Create entity of Factorial.
+     */
+    @Before
+    public void setup() {
+        factorial = new Factorial();
+    }
+
+    /**
+     * The Test validate calculated factorial for any positive whole number.
+     */
+    @Test
+    public void retrieveFactorial() {
+        assertEquals(120, factorial.retrieveFactorial(5));
+    }
+
+    /**
+     * The Test validate calculated factorial for zero.
+     */
+    @Test
+    public void retrieveFactorialFromZero() {
+        assertEquals(1, factorial.retrieveFactorial(0));
+    }
+
+    /**
+     * The Test validate calculated factorial for negative number.
+     */
+    @Test(expected = RuntimeException.class)
+    public void retrieveFactorialFromNegativeNumber() {
+        factorial.retrieveFactorial(-10);
+    }
+
+
+}
